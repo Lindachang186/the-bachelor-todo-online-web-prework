7dbeac2816d331596b29310bb10b175fab302745
Linda Chang <lindachang186@gmail.com>
2018-10-25 13:54:55 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -51,7 +51,7 @@ def get_average_age_for_season(data, season)
   data.each do |seasons, contestants|
     contestants.each do |person, values|
       binding.pry
-      if seasons 
+      if seasons
         occupations << person["age"]
     end
   end
